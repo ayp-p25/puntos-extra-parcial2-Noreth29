4cd90b3d7e9044e52cfe534bc922a7a0334ce1ff
Noreth29 <noreth.guajardo@iteso.mx>
2025-03-28 16:28:37 -0600

    3 errores

--- digitos_divisores.py
+++ digitos_divisores.py
@@ -1,1 +1,13 @@
 # Codifica aquí tu programa
+entrada=input("Escriba un número: ")
+numero= int(entrada)
+contador= 0
+
+for dato in entrada:
+    if dato != "0":
+        if numero % int(dato) == 0:
+            contador += 1
+
+print(f"El numero {numero} tiene {contador} divisores")
+
+#3 errores= int(entrada), dato != "0" y if numero % int (dato).
